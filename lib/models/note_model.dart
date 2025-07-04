66f7d0ed224bb67121ca2df4561b8c5d4a32328d
Abdllah atef <abdllahatef@gmail.com>
2025-07-04 09:38:08 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String subtitle;
+  final String createdAt;
+  final int color;
+
+  NoteModel({
+    required this.title,
+    required this.subtitle,
+    required this.createdAt,
+    required this.color,
+  });
+}
