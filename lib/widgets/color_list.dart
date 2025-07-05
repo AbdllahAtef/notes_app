0deeb9c3c5aa140e05753ea1830b9d84082fcada
Abdllah atef <abdllahatef@gmail.com>
2025-07-05 14:17:30 +0300

    colors list view done

--- lib/widgets/color_list.dart
+++ lib/widgets/color_list.dart
@@ -2,27 +2,63 @@
 
 import 'package:flutter/material.dart';
 
-
-class ColorList extends StatelessWidget {
+class ColorList extends StatefulWidget {
   const ColorList({super.key});
 
+  @override
+  State<ColorList> createState() => _ColorListState();
+}
+
+class _ColorListState extends State<ColorList> {
+  int currentindex = 0;
+  List<Color> colors = [
+    Colors.purple,
+    Colors.yellow,
+    Colors.green,
+    Colors.red,
+    Colors.pink,
+  ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 68,
       child: ListView.builder(
-        itemBuilder: (context, index) => const ColorItem(),
-        itemCount: 10,
+        itemBuilder:
+            (context, index) => GestureDetector(
+              onTap: () 
+                 {
+                  currentindex = index;
+                  setState(() {});
+                
+              },
+              child: ColorItem(
+                isselected: currentindex == index,
+                color: colors[index],
+              ),
+            ),
+        itemCount: colors.length,
         scrollDirection: Axis.horizontal,
       ),
     );
   }
 }
-class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
 
+class ColorItem extends StatelessWidget {
+  const ColorItem({super.key, required this.isselected, required this.color});
+  final bool isselected;
+  final Color color;
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(backgroundColor: Colors.white, radius: 34);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 6.0),
+      child:
+          isselected
+              ? CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 34,
+                child: CircleAvatar(backgroundColor: color, radius: 32),
+              )
+              : CircleAvatar(backgroundColor: color, radius: 34),
+    );
   }
 }
